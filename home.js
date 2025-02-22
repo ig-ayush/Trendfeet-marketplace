5a25efd0fb6a79e70f8a92167cfdb30a1cda46b8
ig-ayush <igayushacharya@gmail.com>
2025-02-22 22:58:13 +0530

    completed the hero section

---
+++ home.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    document.querySelector('.line1').classList.add('done');
+}, 3000); // After first line
+
+setTimeout(() => {
+    document.querySelector('.line2').classList.add('done');
+}, 6000); // After second line
